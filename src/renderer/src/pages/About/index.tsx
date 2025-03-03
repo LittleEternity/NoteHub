3b761deb15d85671eab02c16a35a63d69288422e
Little Eternity <157413462@qq.com>
2025-03-03 14:48:16 +0800

    增加路由以及 Sass

---
+++ src/renderer/src/pages/About/index.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>This is an Electron application built with React and TypeScript.</p>
+      </div>
+    )
+  }
